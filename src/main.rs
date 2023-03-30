288a43cdfe4a35aa9e3eb08367d377cf5a432d8b
kryptox <jackbest@tutanota.com>
2023-03-30 12:27:01 +0100

    Added another page, starting database integration

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,38 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket_contrib::database;
 use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
+use rocket_contrib::databases::mysql;
 use serde_json::json;
 use std::env;
+
+#[database("jackreminders")]
+struct DatabasePool(mysql::Connection);
 
 #[get("/")]
 fn index() -> Template {
     let context = json!({
-        "title": "My Rust Website"
+        "title": "r.me | Home"
     });
     Template::render("index", &context)
 }
 
-fn main() {
-	let template_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/templates");
-    env::set_current_dir(template_dir).unwrap();
-		
+#[get("/reminders")]
+fn reminders() -> Template {
+	let context = json!({
+		"title": "r.me | Reminders"
+	});
+	Template::render("reminders", &context)
+}
+
+fn main() {	
 	rocket::ignite()
 	.mount("/", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")))
-	.mount("/", routes![index])
+	.mount("/", routes![index, reminders])
 	.attach(Template::fairing())
+	.attach(dbConnection::fairing())
+	.manage(mysql::Client::open("mysql://root:password@localhost/jackreminders").unwrap())
 	.launch();
 }
