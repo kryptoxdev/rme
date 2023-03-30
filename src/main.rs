4428e9c313d9d9d2822aac6020ea4a7e809b74d9
kryptox <jackbest@tutanota.com>
2023-03-30 18:47:51 +0100

    Succesfully connected a database

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use rocket_contrib::database;
+use mysql::*;
+use mysql::prelude::*;
 use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
-use rocket_contrib::databases::mysql;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::env;
 
-#[database("jackreminders")]
-struct DatabasePool(mysql::Connection);
+#[derive(Serialize, Deserialize)]
+struct Reminder {
+    id: i32,
+    title: String,
+    description: String,
+    date: String,
+}
 
 #[get("/")]
 fn index() -> Template {
@@ -22,18 +29,29 @@ fn index() -> Template {
 
 #[get("/reminders")]
 fn reminders() -> Template {
-	let context = json!({
-		"title": "r.me | Reminders"
-	});
-	Template::render("reminders", &context)
+    // Set up connection pool
+    let url = "mysql://root:password@localhost:3306/jackreminders";
+    let pool = Pool::new(url).unwrap();
+
+    // Get a connection from the pool
+    let mut conn = pool.get_conn().unwrap();
+
+    let result_set = conn .query_map("SELECT * FROM reminders", |(id, title, description, date)| 
+		Reminder { id, title, description, date} 
+	).unwrap();
+
+    let context = json!({
+        "pagetitle": "r.me | Reminders",
+        "reminders": result_set,
+    });
+
+    Template::render("reminders", &context)
 }
 
-fn main() {	
-	rocket::ignite()
-	.mount("/", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")))
-	.mount("/", routes![index, reminders])
-	.attach(Template::fairing())
-	.attach(dbConnection::fairing())
-	.manage(mysql::Client::open("mysql://root:password@localhost/jackreminders").unwrap())
-	.launch();
+fn main() {
+    rocket::ignite()
+        .mount("/", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")))
+        .mount("/", routes![index, reminders])
+        .attach(Template::fairing())
+        .launch();
 }
